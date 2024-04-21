f45914981d64f680b5200e0f25acfee67c715446
ashley <25075152@edgehill.ac.uk>
2024-04-21 10:59:10 +0000

    replaced div with span after testing in nav button / removed inter fonts from auto applying

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,6 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/navBar"
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Formula S concept racing sport",
@@ -13,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <NavBar />
         {children}
       </body>
